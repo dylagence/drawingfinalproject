deeb65023220aa87e88c4c107350015bebda2710
Dylan Colby <dmc680@nyu.edu>
2019-05-07 05:15:02 -0400

    finished scripting

--- buildings/script.js
+++ buildings/script.js
@@ -1,10 +1,10 @@
+var rCounter = 112; //70b2ff
+var gCounter = 178; //70b2ff
+var bCounter = 255; //70b2ff
+
 setTimeout(function(){
   var a = document.getElementsByClassName("building");
   var delta, buildingWindow, randNum;
-  document.getElementById("background").style.backgroundColor = "rgb("+rCounter+","+gCounter+","+bCounter+")";
-  var rCounter = 80; //70b2ff
-  var gCounter = 131; //70b2ff
-  var bCounter = 191; //70b2ff
   var svgDoc = a[0].contentDocument;
   delta = svgDoc.getElementsByClassName("window");
   for (var i=0;i<delta.length;i++) {
@@ -95,8 +95,43 @@ setTimeout(function(){
         delta[i].addEventListener("mousedown",function() {lightOn(buildingWindow);}, false);
       }());
   };
-  document.getElementById("background").style.backgroundColor = "rgb("+rCounter+","+gCounter+","+bCounter+")";
+  colorChange(rCounter,gCounter,bCounter);
+
 }, 100);
+
+var r = 112;
+var g = 178;
+var b = 255;
+
+function colorChange (rCounter,gCounter,bCounter) {
+  setTimeout(function() {
+    console.log(rCounter,gCounter,bCounter);
+    console.log(r,g,b);
+    if (r > rCounter) {
+      r -= 1;
+    }
+    else if (r != rCounter) {
+      r += 1;
+    }
+    if (g > gCounter) {
+      g -= 1;
+    }
+    else if (g != gCounter) {
+      g += 1;
+    }
+    if (b > bCounter) {
+      b -= 1;
+    }
+    else if (b != bCounter) {
+      b += 1;
+    }
+
+    if (b != bCounter) {
+      document.getElementById("background").style.backgroundColor = "rgb("+r+","+g+","+b+")";
+      colorChange(rCounter,gCounter,bCounter);
+    }
+  }, 50)
+}
 
 function lightOn(buildingWindow) {
   if (buildingWindow.style.fill == "") {
@@ -104,13 +139,13 @@ function lightOn(buildingWindow) {
     rCounter -= 3;
     gCounter -= 3;
     bCounter -= 3;
-    document.getElementById("background").style.backgroundColor = "rgb("+rCounter+","+gCounter+","+bCounter+")";
+    colorChange(rCounter,gCounter,bCounter);
   }
   else if (buildingWindow.style.fill == "rgb(253, 255, 206)") {
     buildingWindow.style.fill = "";
     rCounter += 3;
     gCounter += 3;
     bCounter += 3;
-    document.getElementById("background").style.backgroundColor = "rgb("+rCounter+","+gCounter+","+bCounter+")";
+    colorChange(rCounter,gCounter,bCounter);
   }
 }
